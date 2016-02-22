f51ae458c30cf17966e617a82754019ac6051582
erikperin <erikperin.photo@gmail.com>
2016-02-22 20:03:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-
-
 include("modulos/metadados/cabecalho.php");
 include("modulos/navegacao/nav.php");
 include("modulos/post/post-form.php");
 include("modulos/timeline/lista.php");
 include("modulos/rodape/rodape.php");
-
 ?>
